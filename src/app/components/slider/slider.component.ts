4e2f8967070bc63901d2730cb1eb998e539fca62
jonaswouterscompass <jonas.wouters@axxes.com>
2024-10-04 15:12:55 +0200

    Fullscreen

--- src/app/components/slider/slider.component.ts
+++ src/app/components/slider/slider.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ImageService } from '../../services/image.service';
 import { SecurityService } from '../../services/security.service';
+import { FullscreenService } from '../../services/fullscreen.service';
 
 @Component({
   selector: 'app-slider',
@@ -8,5 +9,9 @@ import { SecurityService } from '../../services/security.service';
   styleUrl: './slider.component.scss'
 })
 export class SliderComponent {
-  constructor(public readonly imageService: ImageService, public readonly securityService: SecurityService) {}
+  constructor(
+    public readonly imageService: ImageService, 
+    public readonly securityService: SecurityService, 
+    public readonly fullscreenService: FullscreenService
+  ) {}
 }
