ee969a2bbec0383c11862e4daec2680af376753b
jonaswouterscompass <jonas.wouters@axxes.com>
2024-10-04 15:24:30 +0200

    fullscreen

--- src/app/services/fullscreen.service.ts
+++ src/app/services/fullscreen.service.ts
@@ -4,6 +4,32 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class FullscreenService {
-  isFullscreen: boolean = true;
+  isFullscreen: boolean = false;
   constructor() { }
+
+  toggleFullscreen(): void {
+    if (this.isFullscreen) {
+      this.closeFullscreen();
+    } else {
+      this.openFullscreen();
+    }
+  }
+
+  openFullscreen(): void {
+    const elem = document.documentElement;
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    }
+    this.isFullscreen = true;
+  }
+
+  closeFullscreen(): void {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+    this.isFullscreen = false;
+  }
+  isFullscreenEnabled(): boolean {
+    return document.fullscreenEnabled;
+  }
 }
